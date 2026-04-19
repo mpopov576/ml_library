963031514b7ab812152a58b6df6b91ff8a20db2d
Miroslav Popov <miroslavpopov576@gmail.com>
2026-04-19 23:33:07 +0300

    Update metrics.py
    made the parameters in r2_score from python lists to numpy arrays

--- metrics.py
+++ metrics.py
@@ -13,6 +13,8 @@ def manhattan_distance(a, b):
     return np.sum(np.abs(a-b))
 
 def r2_score(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     errm =  np.sum((y_true - np.mean(y_true))**2)
     errp = np.sum((y_true-y_pred)**2)
     if errp == 0 and errm == 0 :
