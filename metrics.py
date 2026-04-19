e96b385714764cbdc92193030d863bc1ebec537c
Miroslav Popov <miroslavpopov576@gmail.com>
2026-04-19 19:05:37 +0300

    Update metrics.py
    added euclidean_distance function

--- metrics.py
+++ metrics.py
@@ -1,7 +1,10 @@
-
+import numpy as np
 
 def accuracy_score(y_true, y_pred, normalize=True):
     if normalize:
         return (y_true==y_pred).sum()/y_true.size
 
     return (y_true==y_pred).sum()
+
+def euclidean_distance(a, b):
+    return np.sqrt(np.sum((a-b)**2))
