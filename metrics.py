6bad089192e9d7a7bd24f4425f6243b663cd4ec0
Miroslav Popov <miroslavpopov576@gmail.com>
2026-04-19 23:36:13 +0300

    Update metrics.py
    Add rmse function

--- metrics.py
+++ metrics.py
@@ -17,9 +17,16 @@ def r2_score(y_true, y_pred):
     y_pred = np.asarray(y_pred)
     errm =  np.sum((y_true - np.mean(y_true))**2)
     errp = np.sum((y_true-y_pred)**2)
+    
     if errp == 0 and errm == 0 :
         return 1
     elif errm  == 0:
         return 0
     return 1-errp/errm
+
+def root_mean_squared_error(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    
+    return np.sqrt(np.sum((y_true-y_pred)**2)/len(y_true))
     
