7a2354f1b2dd6bd0d0281cd35cc1b3fc93e87d32
Miroslav Popov <miroslavpopov576@gmail.com>
2026-04-20 12:02:36 +0300

    Update stats.py
    Add sigmoid and softmax functions

--- stats.py
+++ stats.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+def sigmoid(X):
+    return 1 / (1 + np.exp(-X))
+
+def softmax(X):
+    X=np.asarray(X)
+
+    #for numerical stability
+    X_shifted = X - np.max(X, axis=1, keepdims=True)
+
+    return np.exp(X_shifted) / np.sum(np.exp(X_shifted), axis=1, keepdims=True)
